bff00871984a5c3f370eada2b281fd0873d899f8
braianCg <166315887+braianCg@users.noreply.github.com>
2025-02-04 20:05:25 -0300

    Initial commit

---
+++ src/app/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import { ProductsRouter } from '../routes/products.js'
+import { CartRouter } from '../routes/cart.js'
+import { config } from '../routes/index.js'
+
+const initApp = () => {
+    const app = express()
+
+    app.use(express.json())
+    app.use(express.urlencoded())
+    app.use('/', express.static(config.dirname + 'public'))
+    app.use(logger)
+
+app.use('/api/products', ProductsRouter)
+app.use('/api/cart', CartRouter)
+
+
+    console.log(config.dirname)
+    return app
+}
+
+export default initApp
