dc7cdef1c0a4a3eadad630186aae6fd6a8a21c96
braianCg <166315887+braianCg@users.noreply.github.com>
2025-02-18 17:37:29 -0300

    correcciones de errores

--- src/app/index.js
+++ src/app/index.js
@@ -1,15 +1,24 @@
+// filepath: /c:/Users/thela/OneDrive/Escritorio/back/src/app/index.js
 import express from 'express';
+import { create } from 'express-handlebars';
 import { ProductsRouter } from '../routes/products.js';
 import { CartRouter } from '../routes/cart.js';
 import { config } from '../routes/index.js';
-import  logger from '../middlewares/logger.js';
+import logger from '../middlewares/logger.js';
+import { WebSocketServer } from 'ws';
 
 const initApp = () => {
     const app = express();
 
+    // Config de hb
+    const hbs = create({ extname: '.hbs' });
+    app.engine('.hbs', hbs.engine);
+    app.set('view engine', '.hbs');
+    app.set('views', path.join(config.dirname, 'views'));
+
     app.use(express.json());
-    app.use(express.urlencoded());
-    app.use('/', express.static(config.dirname + 'public'));
+    app.use(express.urlencoded({ extended: true }));
+    app.use('/', express.static(path.join(config.dirname, 'public')));
     app.use(logger);
 
     app.use('/api/products', ProductsRouter);
@@ -19,4 +28,24 @@ const initApp = () => {
     return app;
 };
 
-export default initApp;
+const app = initApp();
+
+//config de webS
+const server = app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (ws) => {
+    console.log('New client connected');
+    ws.on('message', (message) => {
+        console.log(`Received message: ${message}`);
+    });
+
+    ws.on('close', () => {
+        console.log('Client disconnected');
+    });
+});
+
+export default app;
